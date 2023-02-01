f17eef9d2f45434646906deb6cf4504c0e08bc9f
R2H <r2h@Administrators-MacBook-Pro.local>
2023-02-01 13:00:37 -0500

    updated css

--- MentalForm.jsx
+++ MentalForm.jsx
@@ -48,7 +48,7 @@ function MentalForm() {
                     <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-purple outline-none focus:outline-none">
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-ally2 outline-none focus:outline-none">
                                 {/*header*/}
                                 <div className="flex items-start justify-between p-3 border-b border-solid border-slate-200 rounded-t">
                                     <h3 className="text-2xl font-semibold">
--- Team.jsx
+++ Team.jsx
@@ -13,7 +13,7 @@ export const Team = () => {
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                 <div>
-                    <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-ally8 uppercase rounded-full bg-ally8">
+                    <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-black uppercase rounded-full bg-ally8">
                         Mentally Team
                     </p>
                 </div>
